8c5310437e80318ef838676a4271652846d67945
plusbeauxjours <plusbeauxjours@gmail.com>
2018-12-19 22:08:20 +0700

    #2.9 Global Styles Set Up

--- src/global-styles.ts
+++ src/global-styles.ts
@@ -1,6 +1,24 @@
 import { createGlobalStyle } from "src/typed-components";
 import reset from "styled-reset";
 
-const GlobalStyle = createGlobalStyle`${reset}`;
+const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css?family=Maven+Pro');
+    ${reset};
+    * {
+        box-sizing: border-box;
+    }
+    body{
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+    a{ 
+        color:inherit;
+        text-decoration:none;
+    }
+    input,
+    button{
+        &:focus,
+        &:active{outline:none}
+    }
+`;
 
 export default GlobalStyle;
