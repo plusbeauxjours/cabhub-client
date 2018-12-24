54c24fea772425f9047fa9603056a31d5f301bd9
plusbeauxjours <plusbeauxjours@gmail.com>
2018-12-24 18:58:17 +0700

    #2.24 VerifyPhone Mutation part One

--- src/Routes/VerifyPhone/VerifyPhoneContainer.tsx
+++ src/Routes/VerifyPhone/VerifyPhoneContainer.tsx
@@ -1,28 +1,41 @@
 import React from "react";
 import VerifyPhonePresenter from "./VerifyPhonePresenter";
 import { RouteComponentProps } from "react-router";
+import { Mutation } from "react-apollo";
+import { verifyPhone, verifyPhoneVariables } from "../../types/api";
+import { VERIFY_PHONE } from "./VerifyPhoneQuereis";
 
 interface IState {
   key: string;
+  phoneNumber: string;
 }
 
 interface IProps extends RouteComponentProps<any> {}
 
+class VerifyMuataion extends Mutation<verifyPhone, verifyPhoneVariables> {}
+
 class VerifyPhoneContainer extends React.Component<IProps, IState> {
-  public state = {
-    key: ""
-  };
   constructor(props: IProps) {
     super(props);
     if (!props.location.state) {
       props.history.push("/");
     }
+    this.state = {
+      key: "",
+      phoneNumber: props.location.state.phone
+    };
   }
   public render() {
-    const { key } = this.state;
-    return <VerifyPhonePresenter onChange={this.onInputChange} key={key} />;
+    const { key, phoneNumber } = this.state;
+    return (
+      <VerifyMuataion mutation={VERIFY_PHONE} variables={{ key, phoneNumber }}>
+        {(mutation, { loading }) => (
+          <VerifyPhonePresenter onChange={this.onInputChange} key={key} />
+        )}
+      </VerifyMuataion>
+    );
   }
-  public onInputChange = event => {
+  public onInputChange: React.ChangeEventHandler<HTMLInputElement> = event => {
     const {
       target: { name, value }
     } = event;
