b44c66b3741dcfbfbd390e145d3af973b6f1954a
plusbeauxjours <plusbeauxjours@gmail.com>
2018-12-26 15:54:48 +0700

    #2.27 SocialLogin with Apollo part One

--- src/Routes/SocialLogin/SocialLoginContainer.tsx
+++ src/Routes/SocialLogin/SocialLoginContainer.tsx
@@ -1,9 +1,31 @@
 import React from "react";
 import SocialLoginPresenter from "./SocialLoginPresenter";
+import { Mutation } from "react-apollo";
+import { facebookConnect, facebookConnectVariables } from "../../types/api";
+import { FACEBOOK_CONNECT } from "./SocialLoginQueries";
+import { RouteChildrenProps } from "react-router";
 
-class SocialLoginContainer extends React.Component {
+class LoginMutaion extends Mutation<
+  facebookConnect,
+  facebookConnectVariables
+> {}
+
+interface IProps extends RouteChildrenProps<any> {}
+
+interface IState {
+  firstName: string;
+  lastName: string;
+  emgil?: string;
+  fbId: string;
+}
+
+class SocialLoginContainer extends React.Component<IProps, IState> {
   public render() {
-    return <SocialLoginPresenter />;
+    return (
+      <LoginMutaion mutation={FACEBOOK_CONNECT}>
+        <SocialLoginPresenter />;
+      </LoginMutaion>
+    );
   }
 }
 
