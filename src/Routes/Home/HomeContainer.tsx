ac9fa437674d1fc31a4bf7f938910e8fa4b1e7d8
plusbeauxjours <plusbeauxjours@gmail.com>
2018-12-26 17:49:58 +0700

    #2.31 Home Sidebar Component

--- src/Routes/Home/HomeContainer.tsx
+++ src/Routes/Home/HomeContainer.tsx
@@ -1,10 +1,30 @@
 import React from "react";
 import HomePresenter from "./HomePresenter";
+import { RouteComponentProps } from "react-router";
 
-class HomeContainer extends React.Component {
+interface IState {
+  isMenuOpen: boolean;
+}
+
+interface IProps extends RouteComponentProps<any> {}
+
+class HomeContainer extends React.Component<IProps, IState> {
+  public state = {
+    isMenuOpen: true
+  };
   public render() {
-    return <HomePresenter />;
+    const { isMenuOpen } = this.state;
+    return (
+      <HomePresenter isMenuOpen={isMenuOpen} toggleMenu={this.toggleMenu} />
+    );
   }
+  public toggleMenu = () => {
+    this.setState(state => {
+      return {
+        isMenuOpen: !state.isMenuOpen
+      };
+    });
+  };
 }
 
 export default HomeContainer;
--- src/Routes/Home/index.tsx
+++ src/Routes/Home/index.tsx
@@ -1,2 +1,2 @@
-import HomeContainer from "./HomePresenter";
+import HomeContainer from "./HomeContainer";
 export default HomeContainer;
