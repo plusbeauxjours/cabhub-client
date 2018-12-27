f4d9a222189bd9440ed9993e2ea89132ef56aaa7
plusbeauxjours <plusbeauxjours@gmail.com>
2018-12-27 23:28:06 +0700

    #2.35 Updating Driver Mode part Two

--- src/Components/Menu/MenuContainer.tsx
+++ src/Components/Menu/MenuContainer.tsx
@@ -4,9 +4,9 @@ import { Query, Mutation } from "react-apollo";
 import { USER_PROFILE } from "../../sharedQueries";
 import { toggleDriving, userProfile } from "../../types/api";
 import { TOGGLE_DRIVING } from "./MenuQueries";
+import { toast } from "react-toastify";
 
 class ProfilQuery extends Query<userProfile> {}
-
 class ToggleDrivingMutation extends Mutation<toggleDriving> {}
 
 class MenuContainer extends React.Component {
@@ -14,7 +14,27 @@ class MenuContainer extends React.Component {
     return (
       <ToggleDrivingMutation
         mutation={TOGGLE_DRIVING}
-        refetchQueries={[{ query: USER_PROFILE }]}
+        update={(cache, { data }) => {
+          if (data) {
+            const { ToggleDrivingMode } = data;
+            if (!ToggleDrivingMode.ok) {
+              toast.error(ToggleDrivingMode.error);
+              return;
+            }
+            const query: userProfile | null = cache.readQuery({
+              query: USER_PROFILE
+            });
+            if (query) {
+              const {
+                GetMyProfile: { user }
+              } = query;
+              if (user) {
+                user.isDriving = !user.isDriving;
+              }
+            }
+            cache.writeQuery({ query: USER_PROFILE, data: query });
+          }
+        }}
       >
         {toggleDrivingFn => (
           <ProfilQuery query={USER_PROFILE}>
