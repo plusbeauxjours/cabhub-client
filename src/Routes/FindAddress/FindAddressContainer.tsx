6432b6162e5fb3f73623fdf093c8291a188e206d
plusbeauxjours <plusbeauxjours@gmail.com>
2018-12-31 22:10:25 +0700

    #2.47 Google Maps and Geolocation

--- src/Routes/FindAddress/FindAddressContainer.tsx
+++ src/Routes/FindAddress/FindAddressContainer.tsx
@@ -10,14 +10,38 @@ class FindAddressContainer extends React.Component<any> {
     this.mapRef = React.createRef();
   }
   public componentDidMount() {
-    const { google } = this.props;
-    const maps = google.maps;
-    const mapNode = ReactDOM.findDOMNode(this.mapRef.current);
-    this.map = new maps.Map(mapNode);
+    navigator.geolocation.getCurrentPosition(
+      this.handleGeoSucces,
+      this.handleGeoError
+    );
   }
   public render() {
     return <FindAddressPresenter mapRef={this.mapRef} />;
   }
+  public handleGeoSucces = (position: Position) => {
+    const {
+      coords: { latitude, longitude }
+    } = position;
+    console.log(position);
+    this.loadMap(latitude, longitude);
+  };
+  public handleGeoError = () => {
+    console.log("No location");
+  };
+  public loadMap = (lat, lng) => {
+    const { google } = this.props;
+    const maps = google.maps;
+    const mapNode = ReactDOM.findDOMNode(this.mapRef.current);
+    const mapConfig: google.maps.MapOptions = {
+      center: {
+        lat,
+        lng
+      },
+      disableDefaultUI: true,
+      zoom: 11
+    };
+    this.map = new maps.Map(mapNode, mapConfig);
+  };
 }
 
 export default FindAddressContainer;
